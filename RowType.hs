dea9a9d94119526358a8148bb738577640308c21
Rudi Horn <dyn-git@rudi-horn.de>
2020-04-29 16:47:41 +0000

    Removed Value from RowType

--- RowType.hs
+++ RowType.hs
@@ -22,7 +22,7 @@ import qualified Types
 import qualified Value
 import qualified Types as T
 
-type Env = [(Symbol, Types.Type)]
+type Env = [(Symbol, *)]
 
 class RecoverEnv e where
   recover_env :: Proxy e -> [(String, Types.Type)]
@@ -47,8 +47,8 @@ data InEnvEvid where
   Take :: InEnvEvid
   Skip :: InEnvEvid -> InEnvEvid
 
-type family LookupType (env :: Env) (s :: Symbol) :: Types.Type where
-  LookupType '[] _ = 'Types.Int
+type family LookupType (env :: Env) (s :: Symbol) :: * where
+  LookupType '[] _ = Int
   LookupType ('(key, val) ': xs) key = val
   LookupType (_ ': xs) key = LookupType xs key
 
@@ -66,7 +66,7 @@ type family Find (env :: Env) (s :: Symbol) :: InEnvEvid where
 
 data Row (e :: Env) where
   Empty :: Row '[]
-  Cons :: Value.Value typ -> Row env -> Row ('( key, typ) ': env)
+  Cons :: t -> Row env -> Row ('( key, t) ': env)
 
 -- Make
 
@@ -82,7 +82,7 @@ class Fields (e :: Env) where
 instance Fields '[] where
   fields RowType.Empty = []
 
-instance (KnownSymbol k, Show (Value.Value t), Fields xs) => Fields ( '(k, t) ': xs) where
+instance (KnownSymbol k, Show t, Fields xs) => Fields ( '(k, t) ': xs) where
   fields (Cons v r) = (symbolVal (Proxy :: Proxy k) ++ " = " ++ show v) : fields r
 
 instance Fields e => Show (Row e) where
@@ -94,7 +94,7 @@ instance Fields e => Show (Row e) where
 -- Row Fetching
 
 class FetchRow t (i :: InEnvEvid) r where
-  intfetch :: r -> Value.Value t
+  intfetch :: r -> t
 
 instance FetchRow t 'Take (Row ('(s, t) ': env)) where
   intfetch (Cons v _) = v
@@ -107,28 +107,24 @@ type Fetchable s env t evid = (
   evid ~ Find env s,
   FetchRow t evid (Row env))
 
-fetchv :: forall s env t evid. Fetchable s env t evid => (Row env) -> Value.Value t
-fetchv row = intfetch @(LookupType env s) @(Find env s) row
-
-fetch :: forall s env t evid. Fetchable s env t evid =>
-  (Row env) -> Types.HaskellType t
-fetch row = Value.valof (intfetch @t @evid row)
+fetch :: forall s env t evid. Fetchable s env t evid => (Row env) -> t
+fetch row = intfetch @t @evid row
 
 
 
 -- Row Updating
-type family UpdateType (s :: Symbol) (t :: Types.Type) (env :: Env) :: Env where
+type family UpdateType (s :: Symbol) (t :: *) (env :: Env) :: Env where
   UpdateType _ _ '[] = '[]
   UpdateType s t ('(s, _) ': env) = '(s, t) ': env
   UpdateType s t ('(k,v) ': env) = '(k, v) ': (UpdateType s t env)
 
-type family SetType (s :: Symbol) (t :: Types.Type) (env :: Env) (i :: Maybe InEnvEvid) :: Env where
+type family SetType (s :: Symbol) (t :: *) (env :: Env) (i :: Maybe InEnvEvid) :: Env where
   SetType s t env 'Nothing = '(s, t) ': env
   SetType s t ('(k, v) ': env) ('Just 'Take) = '(s, t) ': env
   SetType s t ('(k, v) ': env) ('Just ('Skip ev)) = '(k, v) ': (SetType s t env ('Just ev))
 
-class UpdateRow s (t :: Types.Type) (r :: Env) (i :: Maybe InEnvEvid) where
-  intupdate :: Value.Value t -> Row r -> Row (SetType s t r i)
+class UpdateRow s (t :: *) (r :: Env) (i :: Maybe InEnvEvid) where
+  intupdate :: t -> Row r -> Row (SetType s t r i)
 
 instance UpdateRow s t ('(s, t1) ': env) ('Just 'Take) where
   intupdate v (Cons _ env) = Cons v env
@@ -139,16 +135,10 @@ instance (UpdateRow s t env ('Just ev)) => UpdateRow s t ('(k, v) ': env) ('Just
   intupdate v (Cons w row :: Row ('(k, v) ': env)) = Cons w (intupdate @s @t @env @('Just ev) v row) :: Row ('(k, v) ': SetType s t (env) ('Just ev))
 
 instance UpdateRow s t env 'Nothing where
-  intupdate (v :: Value.Value t) row = Cons v row :: Row ('(s, t) ': env)
+  intupdate v row = Cons v row :: Row ('(s, t) ': env)
 
-update :: forall s t env. (UpdateRow s (Types.LensType t) env (FindMaybe env s), Value.MakeValue t) => t -> Row env -> Row (SetType s (Types.LensType t) env (FindMaybe env s))
-update v row = intupdate @s @(Types.LensType t) @env @(FindMaybe env s) (Value.make v) row
-
-update_str :: forall s env. (UpdateRow s 'Types.String env (FindMaybe env s)) => String -> Row env -> Row (SetType s 'Types.String env (FindMaybe env s))
-update_str v row = update @s v row
-
-update_int :: forall s env. (UpdateRow s 'Types.Int env (FindMaybe env s)) => Int -> Row env -> Row (SetType s 'Types.Int env (FindMaybe env s))
-update_int v row = update @s v row
+update :: forall s t env. (UpdateRow s t env (FindMaybe env s)) => t -> Row env -> Row (SetType s t env (FindMaybe env s))
+update v row = intupdate @s @t @env @(FindMaybe env s) v row
 
 class IntRevisable (rt :: Env) (rt' :: Env) (i :: Maybe InEnvEvid) where
   intrevise :: Row rt -> Row rt' -> Row rt
@@ -185,7 +175,7 @@ instance Project '[] env where
   project _ = RowType.Empty
 
 instance (Project xs env, Fetchable x env t evid) => Project (x ': xs) (env) where
-  project r = Cons (fetchv @x r) (project @xs @env r)
+  project r = Cons (fetch @x r) (project @xs @env r)
 
 type Normalisable (e :: Env) = Project (SymAsSet (VarsEnv e)) e
 
@@ -221,14 +211,14 @@ append (Cons v rt) rt' = Cons v (append rt  rt')
 -- Examples
 
 
-row1 :: Row '[ '( "A", 'Types.Int), '("B", 'Types.String)]
-row1 = Cons (Value.Int 5) (Cons (Value.String "h") RowType.Empty)
+row1 :: Row '[ '( "A", Int), '("B", String)]
+row1 = Cons 5 $ Cons "h" RowType.Empty
 
-row2 :: Row '[ '("B", 'Types.String)]
-row2 = Cons (Value.String "h") RowType.Empty
+row2 :: Row '[ '("B", String)]
+row2 = Cons "h" RowType.Empty
 
-row3 :: Row '[ '( "A", 'Types.Int), '("C", 'Types.Bool), '("B", 'Types.String)]
-row3 = Cons (Value.Int 5) (Cons (Value.Bool True) (Cons (Value.String "h") RowType.Empty))
+row3 :: Row '[ '( "A", Int), '("C", Bool), '("B", String)]
+row3 = Cons 5 $ Cons True $ Cons "h" RowType.Empty
 
 
 -- FetchRow
@@ -236,59 +226,54 @@ row3 = Cons (Value.Int 5) (Cons (Value.Bool True) (Cons (Value.String "h") RowTy
 instance FromRow (Row '[]) where
   fromRow = return RowType.Empty
 
-instance (Fld.FromField t0, FromRow (Row xs), Value.MakeValue t0, t0 ~ Types.HaskellType t, Types.LensType t0 ~ t) => FromRow (Row ('(k, t) ': xs)) where
-  fromRow = Cons <$> (Value.make @t0 <$> field) <*> fromRow @(Row xs)
+instance (Fld.FromField t, FromRow (Row xs)) => FromRow (Row ('(k, t) ': xs)) where
+  fromRow = Cons <$> field <*> fromRow @(Row xs)
 
 
 -- Equal
 
-instance Eq (Row e) where
+instance Eq (Row '[]) where
   RowType.Empty == RowType.Empty = True
+
+instance (Eq t, Eq (Row env)) => Eq (Row ( '(k, t) ': env)) where
   Cons v vs == Cons v' vs' = v == v' && vs == vs'
 
 
 -- Compare
 
-instance Ord (Row e) where
+instance Ord (Row '[]) where
   compare Empty RowType.Empty = EQ
+
+instance (Ord t, Ord (Row env)) => Ord (Row ('(k, t) ': env)) where
   compare (Cons v vs) (Cons v' vs') = compare v v' <> compare vs vs'
 
 
 -- Helper Syntax
 
 type family TupleType (e :: Env) :: * where
-  TupleType '[ '(k, t)] = Only (T.HaskellType t)
-  TupleType '[ '(k1, t1), '(k2, t2)] = (T.HaskellType t1, T.HaskellType t2)
-  TupleType '[ '(k1, t1), '(k2, t2), '(k3, t3)] =
-    (T.HaskellType t1, T.HaskellType t2, T.HaskellType t3)
-  TupleType '[ '(k1, t1), '(k2, t2), '(k3, t3), '(k4, t4)] =
-    (T.HaskellType t1, T.HaskellType t2, T.HaskellType t3, T.HaskellType t4)
-  TupleType '[ '(k1, t1), '(k2, t2), '(k3, t3), '(k4, t4), '(k5, t5)] =
-    (T.HaskellType t1, T.HaskellType t2, T.HaskellType t3, T.HaskellType t4, T.HaskellType t5)
+  TupleType '[ '(k, t)] = Only (t)
+  TupleType '[ '(k1, t1), '(k2, t2)] = (t1, t2)
+  TupleType '[ '(k1, t1), '(k2, t2), '(k3, t3)] = (t1, t2, t3)
+  TupleType '[ '(k1, t1), '(k2, t2), '(k3, t3), '(k4, t4)] = (t1, t2, t3, t4)
+  TupleType '[ '(k1, t1), '(k2, t2), '(k3, t3), '(k4, t4), '(k5, t5)] = (t1, t2, t3, t4, t5)
 
 class ToRow rt vals where
   toRow :: vals -> Row rt
 
-type VVal v t = (Value.MakeValueEx v t)
+instance ToRow '[ '(k, t)] (Only t) where
+  toRow (Only v) = Cons v Empty
 
-mkval :: VVal v t => v -> Value.Value t
-mkval v = Value.makeEx v
+instance ToRow '[ '(k1, t1), '(k2, t2)] (t1, t2) where
+  toRow (v1, v2) = Cons (v1) $ Cons (v2) Empty
 
-instance VVal v t => ToRow '[ '(k, t)] (Only v) where
-  toRow (Only v) = Cons (mkval v) Empty
+instance ToRow '[ '(k1, t1), '(k2, t2), '(k3, t3)] (t1, t2, t3) where
+  toRow (v1, v2, v3) = Cons v1 $ Cons v2 $ Cons v3 Empty
 
-instance (VVal v1 t1, VVal v2 t2) => ToRow '[ '(k1, t1), '(k2, t2)] (v1, v2) where
-  toRow (v1, v2) = Cons (mkval v1) $ Cons (mkval v2) Empty
+instance ToRow '[ '(k1, t1), '(k2, t2), '(k3, t3), '(k4, t4)] (t1, t2, t3, t4) where
+  toRow (v1, v2, v3, v4) = Cons v1 $ Cons v2 $ Cons v3 $ Cons v4 Empty
 
-instance (VVal v1 t1, VVal v2 t2, VVal v3 t3) => ToRow '[ '(k1, t1), '(k2, t2), '(k3, t3)] (v1, v2, v3) where
-  toRow (v1, v2, v3) = Cons (mkval v1) $ Cons (mkval v2) $ Cons (mkval v3) Empty
-
-instance (VVal v1 t1, VVal v2 t2, VVal v3 t3, VVal v4 t4) => ToRow '[ '(k1, t1), '(k2, t2), '(k3, t3), '(k4, t4)] (v1, v2, v3, v4) where
-  toRow (v1, v2, v3, v4) = Cons (mkval v1) $ Cons (mkval v2) $ Cons (mkval v3) $ Cons (mkval v4) Empty
-
-instance (VVal v1 t1, VVal v2 t2, VVal v3 t3, VVal v4 t4, VVal v5 t5) => ToRow '[ '(k1, t1), '(k2, t2), '(k3, t3), '(k4, t4), '(k5, t5)] (v1, v2, v3, v4, v5) where
-  toRow (v1, v2, v3, v4, v5) = Cons (mkval v1) $ Cons (mkval v2) $ Cons (mkval v3) $ Cons (mkval v4) $ Cons (mkval v5) Empty
-
+instance ToRow '[ '(k1, t1), '(k2, t2), '(k3, t3), '(k4, t4), '(k5, t5)] (t1, t2, t3, t4, t5) where
+  toRow (v1, v2, v3, v4, v5) = Cons v1 $ Cons v2 $ Cons v3 $ Cons v4 $ Cons v5 Empty
 
 --fetch :: forall (s :: Symbol) (typ :: Types.Type) (env :: Env).
 --fetch :: forall (s :: Symbol) (typ :: Types.Type) (env :: Env).
